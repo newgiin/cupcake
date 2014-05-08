a675515652dae4648f50bb0536b068c02db8c35f
Andrew Nguyen <atnguyen4@gmail.com>
2014-05-08 11:47:44 -0700

    fix variable leaking. decided there are 1024 bytes in a Kb.

--- chrome/menu.js
+++ chrome/menu.js
@@ -7,14 +7,16 @@ function main() {
     var overall_conns = localStorage.getItem(bp.OVERALL_CONNS_KEY);
     var overall_data_transd = localStorage.getItem(bp.OVERALL_DATA_TRANSD_KEY);
     document.getElementById('overall_conns').innerHTML = overall_conns ? overall_conns : 0;
-    document.getElementById('overall_data_transd').innerHTML = overall_data_transd ? overall_data_transd / 1000 : 0;
+    document.getElementById('overall_data_transd').innerHTML = overall_data_transd ? overall_data_transd / 1024 : 0;
 
     var conns_list = bp.deserialize_stats(localStorage.getItem(bp.HOURLY_CONNS_KEY));
     var data_transd_list = bp.deserialize_stats(localStorage.getItem(bp.HOURLY_DATA_TRANSD_KEY));
 
     var curr_time = (new Date()).getTime();
 
-    var day_conns = day_data_transd = 0;
+    var day_conns = 0;
+    var day_data_transd = 0;
+
     for (var i = 0; i < conns_list.length; i++) {
         if (conns_list[i].timestamp >= curr_time - 1000*60*60*24) {
             day_conns += conns_list[i].count;
@@ -26,6 +28,7 @@ function main() {
             day_data_transd += data_transd_list[i].count;
         }
     }
+
     document.getElementById('day_conns').innerHTML = day_conns;
-    document.getElementById('day_data_transd').innerHTML = day_data_transd / 1000;
+    document.getElementById('day_data_transd').innerHTML = day_data_transd / 1024;
 }
